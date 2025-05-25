3926e8054ce2bcf9e0dd0c9095dab2167225c620
sylv256 <contact@sylv.gay>
2025-05-25 17:03:29 -0400

    refactor: source set stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -133,33 +133,21 @@ loom {
 			sourceSet(sourceSets["client"])
 		}
 	}
-	
-	runs {
-		named("client") {
-			client()
-			configName = "Fabric Client"
-			setSource(sourceSets["test"])
-			ideConfigGenerated(true)
-			vmArgs("-Dmixin.debug.verbose=true", "-Dmixin.debug.export=true")
-		}
-		named("server") {
-			server()
-			configName = "Fabric Server"
-			setSource(sourceSets["test"])
-			ideConfigGenerated(true)
-			vmArgs("-Dmixin.debug.verbose=true", "-Dmixin.debug.export=true")
-		}
-		register("datagen") {
-			server()
-			configName = "Fabric Datagen"
-			setSource(sourceSets["test"])
-			ideConfigGenerated(true)
-			vmArg("-Dfabric-api.datagen")
-			vmArg("-Dfabric-api.datagen.output-dir=${file("../common/src/generated/resources")}")
-			vmArg("-Dfabric-api.datagen.modid=${modId}")
-			runDir("build/datagen")
-		}
+}
+
+sourceSets {
+	getByName("client") {
+		runtimeClasspath += sourceSets["test"].runtimeClasspath
+		runtimeClasspath += sourceSets["test"].output
 	}
+	getByName("main") {
+		runtimeClasspath += sourceSets["test"].runtimeClasspath
+		runtimeClasspath += sourceSets["test"].output
+	}
+}
+
+fabricApi {
+	configureDataGeneration()
 }
 
 java {
